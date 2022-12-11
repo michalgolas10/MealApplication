dd7feb2dc41e2a96bdb720fe8f4bf573ffecf61b
Wiktor Grużewski <shazuyt@gmail.com>
2022-12-11 20:19:38 +0100

    Naprawiony szkielet

---
+++ KuceZBronksuConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KuceZBronksuConsole 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

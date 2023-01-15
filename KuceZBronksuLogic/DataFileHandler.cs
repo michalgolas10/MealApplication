0f79874a68ed8d9fe3dc5ef57f13ef7c03cab7b0
Michał Gołaś <michal.golas.10@gmail.com>
2023-01-15 19:23:10 +0100

    Dodano wczytywanie z plików

---
+++ KuceZBronksuLogic/DataFileHandler.cs
@@ -0,0 +1,23 @@
+﻿using KuceZBronksuDAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace KuceZBronksuLogic
+{
+    public class DataFileHandler
+    {
+        public static void ReadingDataFromFile()
+        {
+            string IngredientsDeserialized = File.ReadAllText(@"C:\Users\Baja\Desktop\jcszr8-KuceZBronksu\flavours.json");
+            var IngredientsList = JsonSerializer.Deserialize<List<Ingredient>>(IngredientsDeserialized);
+            string FlavoursCombinationsDeserialized = File.ReadAllText(@"C:\Users\Baja\Desktop\jcszr8-KuceZBronksu\flavours combinations.json");
+            var FlavourCombinationsList = JsonSerializer.Deserialize<List<FlavoursCombionations>>(FlavoursCombinationsDeserialized);
+        }
+
+    }
+}

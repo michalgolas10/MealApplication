dfb110f13b230edb4a2066a4f854de8470ed99ba
Michał Gołaś <michal.golas.10@gmail.com>
2023-04-05 17:06:08 +0200

    Added Images to Images Folder. Write Phats in jsonfile.

--- KuceZBronksuDAL/Models/Recipe.cs
+++ KuceZBronksuDAL/Models/Recipe.cs
@@ -5,26 +5,26 @@ namespace KuceZBronksuDAL
 {
     public class Recipe : Entity
     {
-        public string Label { get; set; }
+        public string? Label { get; set; }
 
-        public string ShareAs { get; set; }
+        public string? ShareAs { get; set; }
 
-        public List<string> DietLabels { get; set; }
+        public List<string>? DietLabels { get; set; }
 
-        public List<string> HealthLabels { get; set; }
+        public List<string>? HealthLabels { get; set; }
 
-        public List<string> Cautions { get; set; }
+        public List<string>? Cautions { get; set; }
 
-        public List<string> IngredientLines { get; set; }
+        public List<string>? IngredientLines { get; set; }
 
         public List<string>? RecipeSteps { get; set; }
 
         public double Calories { get; set; }
 
-        public List<string> CuisineType { get; set; }
+        public List<string>? CuisineType { get; set; }
 
-        public List<string> MealType { get; set; }
-        public ICollection<User> Users { get; set; }
-        public string Image { get; set; }
+        public List<string>? MealType { get; set; }
+        public ICollection<User>? Users { get; set; }
+        public string? Image { get; set; }
     }
 }

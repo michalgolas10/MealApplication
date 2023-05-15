a33e40018c2662e68239a47e2f18d0ae3dcb8f89
Michał Gołaś <michal.golas.10@gmail.com>
2023-05-15 15:26:03 +0200

    Added Time changing of cyclical email send to admin

--- KuceZBronksuWEB/Controllers/AccountController.cs
+++ KuceZBronksuWEB/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Org.BouncyCastle.Asn1.Pkcs;
 using Hangfire;
+using Microsoft.AspNetCore.Identity.UI.Services;
 
 namespace KuceZBronksuWEB.Controllers
 {
@@ -12,11 +13,13 @@ namespace KuceZBronksuWEB.Controllers
 	{
 		private readonly IUserService _userService;
 		private readonly IRecipeService _recipeService;
+		private readonly ITimeService _timeService;
 
-		public AccountController(IUserService userService, IRecipeService recipeService)
+		public AccountController(IUserService userService, IRecipeService recipeService,ITimeService timeService)
 		{
 			_recipeService = recipeService;
 			_userService = userService;
+			_timeService = timeService;
 		}
 
 		public IActionResult Login()
@@ -61,5 +64,16 @@ namespace KuceZBronksuWEB.Controllers
 			_recipeService.DeleteRecipe(id);
 			return RedirectToAction("ShowRecipeWaitingToBeAdd");
 		}
-	}
+        [Authorize(Roles = "Admin")]
+        public IActionResult ChangeTimeOfEmailSend()
+		{
+			return View();
+		}
+        [Authorize(Roles = "Admin")]
+        public IActionResult ChangeTimeOfCyclicalEmailing(TimeViewModel model)
+		{
+            RecurringJob.AddOrUpdate<ITimeService>("SendEmailToAdmin", service => service.SendEmailToAdmin(), Cron.Daily(model.TimeOfCyclicalEmailing));
+            return RedirectToAction("AdministratorPanel");
+        }
+    }
 }

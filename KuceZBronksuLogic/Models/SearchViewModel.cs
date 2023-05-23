c8374f73dd9c75c821187b3a5782c5b9b8edba66
michal-golas10 <117223298+michalgolas10@users.noreply.github.com>
2023-05-23 03:19:15 +0200

    Deleted required from searchviewmodel

--- KuceZBronksuLogic/Models/SearchViewModel.cs
+++ KuceZBronksuLogic/Models/SearchViewModel.cs
@@ -5,15 +5,12 @@ namespace KuceZBronksuBLL.Models
 {
 	public class SearchViewModel
 	{
-		[Required]
         [DisplayName("Ingredients")]
         public string? IngrediendsList { get; set; }
-		[Required]
         [DisplayName("Kcal Amount")]
         public int? KcalAmount { get; set; }
 		[Required]
 		public IEnumerable<string>? ListOfMealType { get; set; }
-		[Required]
         [DisplayName("Meal Type")]
         public IEnumerable<string>? ListOfEmptyMealType { get; set; }
 	}

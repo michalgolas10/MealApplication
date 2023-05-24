cd9696382242995fdaf4f746854f7aa2fad05553
Grzegorz Kowalaczyk <gregkowalczyk.vlo@gmail.com>
2023-05-24 19:50:01 +0200

    Few changes

--- KuceZBronksuAPI/Controllers/VisitedRecipesController.cs
+++ KuceZBronksuAPI/Controllers/VisitedRecipesController.cs
@@ -20,15 +20,6 @@ namespace KuceZBronksuAPI.Controllers
 		[HttpPost(Name = "AddVisitedRecipes")]
 		public async Task AddAsyncVisit([FromBody] VisitedRecipe lastLoggedReport)
 		{
-			//var existingRow = await _context.VisitedRecipes
-			//	.FirstOrDefaultAsync<VisitedRecipe>(x => x.UserId == lastLoggedReport.UserId);
-
-			//if (existingRow != null)
-			//{
-			//	_context.VisitedRecipes.Update(lastLoggedReport);
-			//	await _context.SaveChangesAsync();
-			//	return;
-			//}
 			await _context.AddAsync(lastLoggedReport);
 			await _context.SaveChangesAsync();
 		}
